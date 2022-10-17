25fc751c512989e48a741519dc472b01ffd4b66f
mrglkExim <m.galko@eximpartners.online>
2022-10-17 13:32:47 +0400

    Pressed the footer to the bottom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,10 @@ import './Footer.scss';
 
 function Footer () {
   return (
-  <div className="footer">
-    Footer
+  <div className="footer container">
+     <div className="footer__inner container__row">
+     Footer
+     </div>
   </div>
   )
 };
