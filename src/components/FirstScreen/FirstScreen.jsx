0277002a0d17c7c0d4f30453becd10bd63dcb6c3
mrglkExim <m.galko@eximpartners.online>
2022-10-21 15:36:49 +0400

    First screen adaptive

--- src/components/FirstScreen/FirstScreen.jsx
+++ src/components/FirstScreen/FirstScreen.jsx
@@ -7,7 +7,7 @@ function FirstScreen () {
     <section className="firstScreen container">
       <div className="firstScreen__inner container__row">
         <h1 className="firstScreen__header">Факторинг для поставщиков <br/> и покупателей</h1>
-        <p className="firstScreen__description">Все виды факторинга, все регионы России,<br/>получите выгодные условия на факторинг.</p>
+        <p className="firstScreen__description">Все виды факторинга, все регионы России, получите выгодные условия на факторинг.</p>
         <div className="firstScreen__buttons">
           <ButtonRequest/>
           <ButtonCalculate/>
