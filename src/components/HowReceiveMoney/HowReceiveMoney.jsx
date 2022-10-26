17fba9dfa2a5d3542ada0c78be445a3e12874490
mrglkExim <m.galko@eximpartners.online>
2022-10-26 19:57:27 +0400

    Started making How get money from Market Page

--- src/components/HowReceiveMoney/HowReceiveMoney.jsx
+++ src/components/HowReceiveMoney/HowReceiveMoney.jsx
@@ -1,10 +1,23 @@
 import './HowReceiveMoney.scss';
+import Step from '../Step/Step';
 
 function HowReceiveMoney () {
   return (
-    <div className="howReceiveMoney">
-    HowReceiveMoney
-    </div>
+    <section className="container">
+      <div className="howReceiveMoney__inner container__row">
+        <h1 className="howReceiveMoney__header">Как получить деньги</h1>
+        <div className="howReceiveMoney__steps">
+          <div className="howReceiveMoney__twoSteps">
+            <Step page="market" text="Оставьте заявку, менеджер свяжется с вами" number="1" highlight={false}/>
+            <Step page="market" text="Получите решение" number="2" highlight={true}/>
+          </div>
+          <div className="howReceiveMoney__twoSteps">
+            <Step page="market" text="Подпишите договор онлайн" number="3" highlight={false}/>
+            <Step page="market" text="Деньги уже на вашем счёте!" number="4" highlight={false}/>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
