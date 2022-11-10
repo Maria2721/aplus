fcc371599e9537f4a4bdf193c6d12850ebbe5ae5
mrglkExim <m.galko@eximpartners.online>
2022-11-10 20:07:36 +0400

    Updated hook for modal to fix sctollbar on Windows

--- src/hooks/useDisableBodyScroll.jsx
+++ src/hooks/useDisableBodyScroll.jsx
@@ -3,10 +3,26 @@ import { useEffect } from 'react';
 export const useDisableBodyScroll = (open) => {
     useEffect(() => {
         if (open) {
+            document.body.style.paddingRight = `${scrollbarWidth()}px`;
             document.body.style.overflow = 'hidden';
             
         } else {
+            document.body.style.paddingRight = '0px';
             document.body.style.overflow = 'unset';
         }
     }, [open]);
 };
+
+const scrollbarWidth = () => {
+	document.body.style.overflow = 'hidden';
+	let width = document.body.clientWidth;
+	document.body.style.overflow = 'scroll';
+ 
+	width -= document.body.clientWidth;
+ 
+	if(!width) width = document.body.offsetWidth-document.body.clientWidth;
+ 
+	document.body.style.overflow = '';
+ 
+	return width;
+}
