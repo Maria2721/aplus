ad94d91dda00fc012b42c2ebb18e0502f3d35a4d
MariaIvanovaEXIM <m.ivanova@eximpartners.online>
2022-11-21 23:29:57 +0300

    add img for retina display in case pages

--- src/pages/OneCasePage/OneCasePage.jsx
+++ src/pages/OneCasePage/OneCasePage.jsx
@@ -5,19 +5,32 @@ import BannerFactoring from '../../components/BannerFactoring/BannerFactoring';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
 
 import fruitdelivery from '../../assets/imgs/fruit-delivery.jpg';
-import seafooddelivery from '../../assets/imgs/seafood-delivery.jpg';
-import softwaredevelopment from '../../assets/imgs/software-development.jpg';
-import itoutsourcing from '../../assets/imgs/it-outsourcing.jpg';
-
+import fruitdeliveryRetina from '../../assets/imgs/fruit-delivery_2x.jpg';
 import fruitdeliveryTablet from '../../assets/imgs/fruit-delivery-tablet.jpg';
-import seafooddeliveryTablet from '../../assets/imgs/seafood-delivery-tablet.jpg';
-import softwaredevelopmentTablet from '../../assets/imgs/software-development-tablet.jpg';
-import itoutsourcingTablet from '../../assets/imgs/it-outsourcing-tablet.jpg';
-
+import fruitdeliveryTabletRetina from '../../assets/imgs/fruit-delivery-tablet_2x.jpg';
 import fruitdeliveryMobile from '../../assets/imgs/fruit-delivery-mobile.jpg';
+import fruitdeliveryMobileRetina from '../../assets/imgs/fruit-delivery-mobile_2x.jpg';
+
+import seafooddelivery from '../../assets/imgs/seafood-delivery.jpg';
+import seafooddeliveryRetina from '../../assets/imgs/seafood-delivery_2x.jpg';
+import seafooddeliveryTablet from '../../assets/imgs/seafood-delivery-tablet.jpg';
+import seafooddeliveryTabletRetina from '../../assets/imgs/seafood-delivery-tablet_2x.jpg';
 import seafooddeliveryMobile from '../../assets/imgs/seafood-delivery-mobile.jpg';
+import seafooddeliveryMobileRetina from '../../assets/imgs/seafood-delivery-mobile_2x.jpg';
+
+import softwaredevelopment from '../../assets/imgs/software-development.jpg';
+import softwaredevelopmentRetina from '../../assets/imgs/software-development_2x.jpg';
+import softwaredevelopmentTablet from '../../assets/imgs/software-development-tablet.jpg';
+import softwaredevelopmentTabletRetina from '../../assets/imgs/software-development-tablet_2x.jpg';
 import softwaredevelopmentMobile from '../../assets/imgs/software-development-mobile.jpg';
+import softwaredevelopmentMobileRetina from '../../assets/imgs/software-development-mobile_2x.jpg';
+
+import itoutsourcing from '../../assets/imgs/it-outsourcing.jpg';
+import itoutsourcingRetina from '../../assets/imgs/it-outsourcing_2x.jpg';
+import itoutsourcingTablet from '../../assets/imgs/it-outsourcing-tablet.jpg';
+import itoutsourcingTabletRetina from '../../assets/imgs/it-outsourcing-tablet_2x.jpg';
 import itoutsourcingMobile from '../../assets/imgs/it-outsourcing-mobile.jpg';
+import itoutsourcingMobileRetina from '../../assets/imgs/it-outsourcing-mobile_2x.jpg';
 
 function OneCasePage(props) {
   const currentCase = cases.find((item) => (item.id === props.case));
@@ -25,23 +38,61 @@ function OneCasePage(props) {
 
   const renderImg = () => {
     if ((width >= 959)) {
-      return <img className="main__schemeImg" alt="Схема"
-        src={currentCase.id === "fruitdelivery" ? fruitdelivery
-          : currentCase.id === "seafooddelivery" ? seafooddelivery
-            : currentCase.id === "softwaredevelopment" ? softwaredevelopment
-              : itoutsourcing} />;
-    } else if ((width < 959) && (width > 480)) {
-      return <img className="main__schemeImg" alt="Схема"
-        src={currentCase.id === "fruitdelivery" ? fruitdeliveryTablet
-          : currentCase.id === "seafooddelivery" ? seafooddeliveryTablet
-            : currentCase.id === "softwaredevelopment" ? softwaredevelopmentTablet
-              : itoutsourcingTablet} />;
-    } else {
-      return <img className="main__schemeImg" alt="Схема"
-        src={currentCase.id === "fruitdelivery" ? fruitdeliveryMobile
-          : currentCase.id === "seafooddelivery" ? seafooddeliveryMobile
-            : currentCase.id === "softwaredevelopment" ? softwaredevelopmentMobile
-              : itoutsourcingMobile} />;
+      if (currentCase.id === "fruitdelivery") {
+        return (
+          <img className="main__schemeImg" alt="Схема"
+            src={fruitdelivery} srcSet={`${fruitdeliveryRetina} 2x`} />)
+      }
+      else if (currentCase.id === "seafooddelivery") {
+        return (<img className="main__schemeImg" alt="Схема"
+          src={seafooddelivery} srcSet={`${seafooddeliveryRetina} 2x`} />)
+      }
+      else if (currentCase.id === "softwaredevelopment") {
+        return (<img className="main__schemeImg" alt="Схема"
+          src={softwaredevelopment} srcSet={`${softwaredevelopmentRetina} 2x`} />)
+      }
+      else if (currentCase.id === "itoutsourcing") {
+        return (<img className="main__schemeImg" alt="Схема"
+          src={itoutsourcing} srcSet={`${itoutsourcingRetina} 2x`} />)
+      }
+    }
+    else if ((width < 959) && (width > 480)) {
+      if (currentCase.id === "fruitdelivery") {
+        return (
+          <img className="main__schemeImg" alt="Схема"
+            src={fruitdeliveryTablet} srcSet={`${fruitdeliveryTabletRetina} 2x`} />)
+      }
+      else if (currentCase.id === "seafooddelivery") {
+        return (<img className="main__schemeImg" alt="Схема"
+          src={seafooddeliveryTablet} srcSet={`${seafooddeliveryTabletRetina} 2x`} />)
+      }
+      else if (currentCase.id === "softwaredevelopment") {
+        return (<img className="main__schemeImg" alt="Схема"
+          src={softwaredevelopmentTablet} srcSet={`${softwaredevelopmentTabletRetina} 2x`} />)
+      }
+      else if (currentCase.id === "itoutsourcing") {
+        return (<img className="main__schemeImg" alt="Схема"
+          src={itoutsourcingTablet} srcSet={`${itoutsourcingTabletRetina} 2x`} />)
+      }
+    }
+    else {
+      if (currentCase.id === "fruitdelivery") {
+        return (
+          <img className="main__schemeImg" alt="Схема"
+            src={fruitdeliveryMobile} srcSet={`${fruitdeliveryMobileRetina} 2x`} />)
+      }
+      else if (currentCase.id === "seafooddelivery") {
+        return (<img className="main__schemeImg" alt="Схема"
+          src={seafooddeliveryMobile} srcSet={`${seafooddeliveryMobileRetina} 2x`} />)
+      }
+      else if (currentCase.id === "softwaredevelopment") {
+        return (<img className="main__schemeImg" alt="Схема"
+          src={softwaredevelopmentMobile} srcSet={`${softwaredevelopmentMobileRetina} 2x`} />)
+      }
+      else if (currentCase.id === "itoutsourcing") {
+        return (<img className="main__schemeImg" alt="Схема"
+          src={itoutsourcingMobile} srcSet={`${itoutsourcingMobileRetina} 2x`} />)
+      }
     }
   }
 
