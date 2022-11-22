ffc5414af89a1927388317c885c33b4995d1e71c
MariaIvanovaEXIM <m.ivanova@eximpartners.online>
2022-11-22 14:59:55 +0300

    added the cases page

---
+++ src/components/CaseBox/CaseBox.jsx
@@ -0,0 +1,56 @@
+import './CaseBox.scss';
+import { Link } from "react-router-dom";
+
+import fruitdelivery from '../../assets/imgs/fruit-delivery_cases.jpg';
+import fruitdeliveryRetina from '../../assets/imgs/fruit-delivery_cases_2x.jpg';
+import seafooddelivery from '../../assets/imgs/seafood-delivery_cases.jpg';
+import seafooddeliveryRetina from '../../assets/imgs/seafood-delivery_cases_2x.jpg';
+import softwaredevelopment from '../../assets/imgs/software-development_cases.jpg';
+import softwaredevelopmentRetina from '../../assets/imgs/software-development_cases_2x.jpg';
+import itoutsourcing from '../../assets/imgs/it-outsourcing_cases.jpg';
+import itoutsourcingRetina from '../../assets/imgs/it-outsourcing_cases_2x.jpg';
+import { ReactComponent as ArrowIcon } from "../../assets/imgs/arrow_more_cases.svg";
+
+function CaseBox({ id, title, subtitle }) {
+
+  const renderImg = () => {
+    if (id === "fruitdelivery") {
+      return (
+        <img className="caseBox__schemeImg" alt="Схема"
+          src={fruitdelivery} srcSet={`${fruitdeliveryRetina} 2x`} />)
+    }
+    else if (id === "seafooddelivery") {
+      return (<img className="caseBox__schemeImg" alt="Схема"
+        src={seafooddelivery} srcSet={`${seafooddeliveryRetina} 2x`} />)
+    }
+    else if (id === "softwaredevelopment") {
+      return (<img className="caseBox__schemeImg" alt="Схема"
+        src={softwaredevelopment} srcSet={`${softwaredevelopmentRetina} 2x`} />)
+    }
+    else if (id === "itoutsourcing") {
+      return (<img className="caseBox__schemeImg" alt="Схема"
+        src={itoutsourcing} srcSet={`${itoutsourcingRetina} 2x`} />)
+    }
+  }
+
+  return (
+    <div className="caseBox" id={id}>
+      <div className="caseBox__scheme">{renderImg()}</div>
+      <div className="caseBox__text">
+        <div className="caseBox__textTitle">{title}</div>
+        <div className="caseBox__textSubtitle">{subtitle}</div>
+        <div className="linkBox">
+          <Link to={id === "fruitdelivery" ? "/case-fruitdelivery"
+            : id === "seafooddelivery" ? "/case-seafooddelivery"
+              : id === "softwaredevelopment" ? "/case-softwaredevelopment"
+                : "/case-itoutsourcing"} className="linkBox__text">
+            Подробнее
+          </Link>
+          <ArrowIcon className="linkBox__arrowIcon" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CaseBox;
--- src/pages/CasesPage/CasesPage.jsx
+++ src/pages/CasesPage/CasesPage.jsx
@@ -1,22 +1,21 @@
 import './CasesPage.scss';
-import { Link } from "react-router-dom";
+import { cases } from '../OneCasePage/cases';
+import CaseBox from '../../components/CaseBox/CaseBox';
 
 const CasesPage = () => (
   <div className="casesPage container">
     <div className="casesPage__inner container__row">
-      <h1>КЕЙСЫ</h1>
-      <Link to="/case-fruitdelivery" className="casesPage__link">
-        Поставка фруктов и овощей
-      </Link>
-      <Link to="/case-seafooddelivery" className="casesPage__link">
-        Поставка морепродуктов
-      </Link>
-      <Link to="/case-softwaredevelopment" className="casesPage__link">
-        Разработка программного обеспечения
-      </Link>
-      <Link to="/case-itoutsourcing" className="casesPage__link">
-        IT-аутсорсинг
-      </Link>
+      <h1 className="casesPage__header">КЕЙСЫ</h1>
+      <div className="casesPage__items">
+        {cases.map((item) => (
+          <CaseBox
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            subtitle={item.subtitle}
+          />
+        ))}
+      </div>
     </div>
   </div>
 );
