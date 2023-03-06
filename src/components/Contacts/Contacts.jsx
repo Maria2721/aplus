15f83af99637355b0c0514a4654a7b789ff55002
MariaIvanovaEXIM <m.ivanova@eximpartners.online>
2023-03-06 14:56:25 +0300

    fixed adress in Contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -12,7 +12,7 @@ function Contacts({ id }) {
           <div className="contacts__contacts">
             <div className="contacts__row">
               <h2 className="contacts__subtitle">Центральный офис</h2>
-              <div className="contacts__description">350059, Россия, <br className="contacts__desktop_hidden" />г. Краснодар,<br />ул. Уральская, д. 75/1</div>
+              <div className="contacts__description">РФ, г. Москва, 123112,<br />Пресненская набережная, д. 12, <br className="contacts__desktop_hidden" />этаж 82, офис 405, <br className="contacts__desktop_hidden" />помещение 3</div>
             </div>
             <div className="contacts__row">
               <h2 className="contacts__subtitle">Для связи</h2>
@@ -27,7 +27,7 @@ function Contacts({ id }) {
             </div>
           </div>
           <div className="contacts__mapWrapper">
-            <iframe className="contacts__map" title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2819.6204862181958!2d39.044901415472644!3d45.03262907909825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40f05aac19194549%3A0x14a99157224e7e5b!2z0YPQuy4g0KPRgNCw0LvRjNGB0LrQsNGPLCA3NS8xLCDQmtGA0LDRgdC90L7QtNCw0YAsINCa0YDQsNGB0L3QvtC00LDRgNGB0LrQuNC5INC60YDQsNC5LCAzNTAwNTk!5e0!3m2!1sru!2sru!4v1666950323683!5m2!1sru!2sru" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" ></iframe>
+            <iframe className="contacts__map" title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2245.6338454771458!2d37.53905011587424!3d55.74749068055193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54bdcef9d88d7%3A0x40c582c3ad3a4bad!2z0JjQvNC_0LXRgNC40Y8sINCf0YDQtdGB0L3QtdC90YHQutCw0Y8g0L3QsNCxLiwgMTIsINCc0L7RgdC60LLQsCwgMTIzMTEy!5e0!3m2!1sru!2sru!4v1678103563569!5m2!1sru!2sru" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </div>
